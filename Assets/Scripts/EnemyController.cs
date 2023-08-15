ae6e83036887090154b30381a54e6641bfa9bf00
i_rk_jain <svsravikiran.9@gmail.com>
2023-08-15 19:13:35 +0530

    Enemy Navigation Trapped and Chaos

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -9,8 +9,9 @@ public class EnemyController : MonoBehaviour {
 
     private int layerMask;
 
-    private Vector3 dir = VectorConstants.RIGHT;
+    private Vector3 dir = Vector3.zero;
     private Vector3 target = Vector3.zero;
+    private bool isTrapped = false;
 
 
     private void Awake() {
@@ -18,39 +19,95 @@ public class EnemyController : MonoBehaviour {
     }
 
     private void Start() {
-        SetTarget();
+        SetNewTarget();
     }
 
     private void Update() {
         transform.position = transform.position + dir * vMax * Time.deltaTime;
-
-        if (Vector3.Dot((target - transform.position), dir) <= 0) {
-            transform.position = target;
-            SetTarget();
-        }
     }
 
-    private void SetTarget() {
-        bool doesIntersect = Physics.Raycast(transform.position, dir, 1, layerMask);
-        Debug.DrawRay(transform.position, dir, Color.white, 0.1f);
+    private void SetNewTarget() {
+        List<Vector3> availableDirs = GetAvailableDirections();
+        if (availableDirs.Count == 0) {
+            // I am trapped => Hence I don't move.
+            dir = Vector3.zero;
 
-        Vector3 newDir = dir;
-        if (doesIntersect) {
-            if (Random.Range(0, 100) < chaosCutoff) {
-                newDir = Quaternion.Euler(0, 90 + 180 * Random.Range(0, 2), 0) * dir;
-                Debug.DrawRay(transform.position, newDir, Color.red, 0.1f);
-
-                if (Physics.Raycast(transform.position, newDir, 1, layerMask)) {
-                    newDir = -dir;
-                }
+        } else {
+            // I have at least one way
+            if (dir.magnitude == 0) {
+                // I wasn't moving previously
+                dir = availableDirs[Random.Range(0, availableDirs.Count)];
 
             } else {
-                newDir = -dir;
+                // I was moving previously
+                if (availableDirs.Contains(dir)) {
+                    // There is a way forward => Hence, I go forward
+
+                    if (availableDirs.Count > 1 && Random.Range(0, 100) < chaosCutoff / 4) {
+                        // if there are other ways and chaos is true
+
+                        availableDirs.Remove(dir);
+                        dir = availableDirs[Random.Range(0, availableDirs.Count)];
+
+                    } else {
+                        // chaos is false or no other way but back
+                        //dir = dir;
+                    }
+                } else {
+                    // There is no way forward
+                    if (availableDirs.Contains(-dir)) {
+                        // There is a way backward => Hence, I go backward
+
+                        if (availableDirs.Count > 1 && Random.Range(0, 100) < chaosCutoff) {
+                            // if there are other ways and chaos is true
+
+                            availableDirs.Remove(-dir);
+                            dir = availableDirs[Random.Range(0, availableDirs.Count)];
+
+                        } else {
+                            // chaos is false or no other way but back
+                            dir = -dir;
+                        }
+
+                    } else {
+                        // There is no way backward
+                        dir = availableDirs[Random.Range(0, availableDirs.Count)];
+                    }
+                }
             }
+
+            Debug.DrawRay(transform.position, dir, Color.white, 0.2f);
         }
 
-        dir = newDir;
         target = transform.position + dir;
+        target.x = Mathf.RoundToInt(target.x);
+        target.z = Mathf.RoundToInt(target.z);
+        target.y = 0;
+
+        StartCoroutine(WaitAndStart(1f / vMax));
+    }
+
+    private IEnumerator WaitAndStart(float t) {
+        yield return new WaitForSeconds(t);
+        transform.position = target;
+        SetNewTarget();
+    }
+
+    private List<Vector3> GetAvailableDirections() {
+        List<Vector3> dirs = new List<Vector3>();
+        if (!Physics.Raycast(transform.position, VectorConstants.UP, 1, layerMask)) {
+            dirs.Add(VectorConstants.UP);
+        }
+        if (!Physics.Raycast(transform.position, VectorConstants.RIGHT, 1, layerMask)) {
+            dirs.Add(VectorConstants.RIGHT);
+        }
+        if (!Physics.Raycast(transform.position, VectorConstants.DOWN, 1, layerMask)) {
+            dirs.Add(VectorConstants.DOWN);
+        }
+        if (!Physics.Raycast(transform.position, VectorConstants.LEFT, 1, layerMask)) {
+            dirs.Add(VectorConstants.LEFT);
+        }
+        return dirs;
     }
 
 }
